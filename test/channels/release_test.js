54a86ac7962dd12fecf90f9d308cf2bd4bbd6ec2
James Lal <james@lightsofapollo.com>
2013-10-08 16:19:09 -0700

    success cases for languages and operating systems

--- test/channels/release_test.js
+++ test/channels/release_test.js
@@ -8,16 +8,46 @@ suite('channels/release', function() {
     }, /os/);
   });
 
-
+  // operating systems
   [
     'linux-i686',
     'linux-x86_64',
-    'mac'
+    'mac',
+    'win32'
   ].forEach(function(os) {
     verifyGet({ os: os }, function() {
-      assert.ok(
+      assert(
         this.url.indexOf(os) !== -1,
         'is for correct os: ' + this.url
+      );
+    });
+  });
+
+  // verify some versions
+  [
+    '3.0',
+    '11.0',
+    '18.0b2',
+    '22.0'
+  ].forEach(function(version) {
+    verifyGet({ os: 'mac', version: version }, function() {
+      assert(
+        this.url.indexOf(version) !== -1,
+        'can request version:' + this.url
+      );
+    });
+  });
+
+  // verify some languages
+  [
+    'fr',
+    'es-ES',
+    'en-US'
+  ].forEach(function(lang) {
+    verifyGet({ os: 'mac', language: lang }, function() {
+      assert(
+        this.url.indexOf(lang) !== -1,
+        'can request lang:' + this.url
       );
     });
   });
