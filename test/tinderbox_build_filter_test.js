a05fa384139530623b2ba32e98379aec0916e2d5
James Lal <james@lightsofapollo.com>
2013-10-09 09:45:29 -0700

    fix firefox supprt for tinderbox builds

--- test/tinderbox_build_filter_test.js
+++ test/tinderbox_build_filter_test.js
@@ -1,45 +1,73 @@
 suite('tinderbox_build_filter', function() {
-  var fixture = require('./fixtures/tinderbox_b2g.json'),
-      subject = require('../lib/tinderbox_build_filter');
+  var subject = require('../lib/tinderbox_build_filter');
 
-  test('mozilla-central mac', function() {
-    var options = { tinderbox: 'mozilla-central', os: 'mac' };
-    assert.equal(
-      subject(options, fixture),
-      'mozilla-central-macosx64_gecko'
-    );
+  suite('product: b2g', function() {
+    var fixture = require('./fixtures/tinderbox_b2g.json');
+    function opts(os) {
+      return {
+        product: 'b2g',
+        tinderbox: 'mozilla-central',
+        os: os
+      };
+    }
+
+    test('mozilla-central mac', function() {
+      assert.equal(
+        subject(opts('mac'), fixture),
+        'mozilla-central-macosx64_gecko'
+      );
+    });
+
+    test('mozilla-central win32', function() {
+      assert.equal(
+        subject(opts('win32'), fixture),
+        'mozilla-central-win32_gecko'
+      );
+    });
+
+    test('mozilla-central linux-i686', function() {
+      assert.equal(
+        subject(opts('linux-i686'), fixture),
+        'mozilla-central-linux32_gecko'
+      );
+    });
+
+    test('mozilla-central linux-x86_64', function() {
+      assert.equal(
+        subject(opts('linux-x86_64'), fixture),
+        'mozilla-central-linux64_gecko'
+      );
+    });
   });
 
-  test('mozilla-central win32', function() {
-    var options = { tinderbox: 'mozilla-central', os: 'win32' };
-    assert.equal(
-      subject(options, fixture),
-      'mozilla-central-win32_gecko'
-    );
-  });
+  suite('product: firefox', function() {
+    var fixture = require('./fixtures/tinderbox_firefox.json');
+    function opts(os) {
+      return {
+        tinderbox: 'mozilla-central',
+        os: os
+      };
+    }
 
-  test('mozilla-central linux-i686', function() {
-    var options = { tinderbox: 'mozilla-central', os: 'linux-i686' };
-    assert.equal(
-      subject(options, fixture),
-      'mozilla-central-linux32_gecko'
-    );
-  });
+    test('mozilla-central mac', function() {
+      assert.equal(
+        subject(opts('mac'), fixture),
+        'mozilla-central-macosx64'
+      );
+    });
 
-  test('mozilla-central linux-x86_64', function() {
-    var options = { tinderbox: 'mozilla-central', os: 'linux-x86_64' };
-    assert.equal(
-      subject(options, fixture),
-      'mozilla-central-linux64_gecko'
-    );
-  });
+    test('mozilla-central win32', function() {
+      assert.equal(
+        subject(opts('win32'), fixture),
+        'mozilla-central-win32'
+      );
+    });
 
-  // sanity check for inbound
-  test('mozilla-inbound linux-i686', function() {
-    var options = { tinderbox: 'mozilla-inbound', os: 'linux-i686' };
-    assert.equal(
-      subject(options, fixture),
-      'mozilla-inbound-linux32_gecko'
-    );
+    test('mozilla-central linux-x86_64', function() {
+      assert.equal(
+        subject(opts('linux-x86_64'), fixture),
+        'mozilla-central-linux64'
+      );
+    });
   });
 });
