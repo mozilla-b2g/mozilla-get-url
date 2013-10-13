de0005eda8088a3d8e0f9025a260bc562e3e6688
James Lal <james@lightsofapollo.com>
2013-10-13 14:25:02 -0700

    Bug 926233 - try server support

---
+++ test/channels/try_test.js
@@ -0,0 +1,17 @@
+suite('channels/release', function() {
+  var get = require('../../lib/channels/try'),
+      opts = require('../../lib/options'),
+      verifyGet = require('../support/get_suite')(get);
+
+  suite('fabrice try', function() {
+    function opts(os) {
+      return {
+        product: 'b2g',
+        branch: 'fdesre@mozilla.com-edae0a5285ac',
+        os: os
+      };
+    }
+
+    verifyGet(opts('linux-x86_64'));
+  });
+});
